9b5a364ed2fbd6c7dd3ab8c88cfb5c7118fce699
Shujaat Azim <shujaat.azim@gmail.com>
2021-12-22 15:16:15 -1000

    Big commit, made major changes to the portfolio data page. Edited package.json to reflect my own information, as well as the html root.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/ShujaatAzim'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Created By Shujaat Azim
     </a>
   </footer>
 )
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://shujaatazim.github.io/',
+  title: 'SA.',
 }
 
 const about = {
@@ -9,11 +9,11 @@ const about = {
   name: 'Shujaat Azim',
   role: 'Full Stack Software Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'I am an enthusiastic full stack software engineer with a background in, and passion for technology, medicine, and finance. I am a seasoned professional with a diverse background in many fields, which enables me to quickly adopt and master new technologies. I am always eager to continue learning and improving!',
+  resume: '',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/shujaatazim/',
+    github: 'https://github.com/ShujaatAzim',
   },
 }
 
@@ -21,51 +21,50 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Pokébook',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Finally! A way to track and share your Pokemon Card collection to help you with trading, selling, and showing off!',
+    stack: ['React', 'Rails', 'Javascript'],
+    sourceCode: 'https://github.com/ShujaatAzim/PokeBook-Frontend',
+    livePreview: 'https://pokebook.shujaatazim.com',
   },
   {
-    name: 'Project 2',
+    name: 'TweedleDoo',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Not just your simple, everyday to-do list! Create multiple lists, fill them with bite-sized tasks, and watch your productivity soar!',
+    stack: ['React', 'Rails', 'Javascript'],
+    sourceCode: 'https://github.com/ShujaatAzim/TweedleDoo-Frontend',
+    livePreview: 'https://tweedledoo.shujaatazim.com',
   },
   {
-    name: 'Project 3',
+    name: 'Projective',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A project management app that breaks down large projects into manageable (and deferable) pieces. Full stack app built just in Rails!',
+    stack: ['Rails', 'Ruby'],
+    sourceCode: 'https://github.com/ShujaatAzim/Projective',
+    livePreview: 'https://projective.shujaatazim.com',
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
+  'React',
+  'Ruby on Rails',
+  'Javascript',
+  'Ruby',
   'HTML',
   'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
   'Redux',
   'SASS',
   'Material UI',
   'Git',
-  'CI/CD',
   'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'shujaatazim@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
