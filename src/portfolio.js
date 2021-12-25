a4861ca8a19d6501224fe398b08d105933b0fb2b
Shujaat Azim <shujaat.azim@gmail.com>
2021-12-24 23:28:43 -1000

    Changed quotes.

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,20 +1,20 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://shujaatazim.github.io/',
-  title: 'SA.',
+  homepage: "https://shujaatazim.github.io/",
+  title: "SA.",
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'Shujaat Azim',
-  role: 'Full Stack Software Engineer',
+  name: "Shujaat Azim",
+  role: "Full Stack Software Engineer",
   description:
-    'I am an enthusiastic full stack software engineer with a background in, and passion for technology, medicine, and finance. I am a seasoned professional with a diverse background in many fields, which enables me to quickly adopt and master new technologies. I am always eager to continue learning and improving!',
-  resume: '',
+    "I am an enthusiastic full stack software engineer with a background in, and passion for technology, medicine, and finance. I am a seasoned professional with a diverse background in many fields, which enables me to quickly adopt and master new technologies. I am always eager to continue learning and improving!",
+  resume: "",
   social: {
-    linkedin: 'https://www.linkedin.com/in/shujaatazim/',
-    github: 'https://github.com/ShujaatAzim',
-    blog: 'https://dev.to/shujaatazim'
+    linkedin: "https://www.linkedin.com/in/shujaatazim/",
+    github: "https://github.com/ShujaatAzim",
+    blog: "https://dev.to/shujaatazim"
   },
 }
 
@@ -22,61 +22,61 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Pokébook',
+    name: "Pokébook",
     description:
-      'Finally! A way to track and share your Pokemon Card collection to help you with trading, selling, and showing off!',
-    stack: ['React', 'Rails', 'Javascript'],
-    sourceCode: 'https://github.com/ShujaatAzim/PokeBook-Frontend',
-    livePreview: 'https://pokebook.shujaatazim.com',
+      "Finally! A way to track and share your Pokemon Card collection to help you with trading, selling, and showing off!",
+    stack: ["React", "Rails", "Javascript"],
+    sourceCode: "https://github.com/ShujaatAzim/PokeBook-Frontend",
+    livePreview: "https://pokebook.shujaatazim.com",
   },
   {
-    name: 'TweedleDoo',
+    name: "TweedleDoo",
     description:
-      'Not just your simple, everyday to-do list! Create multiple lists, fill them with bite-sized tasks, and watch your productivity soar!',
-    stack: ['React', 'Rails', 'Javascript'],
-    sourceCode: 'https://github.com/ShujaatAzim/TweedleDoo-Frontend',
-    livePreview: 'https://tweedledoo.shujaatazim.com',
+      "Not just your simple, everyday to-do list! Create multiple lists, fill them with bite-sized tasks, and watch your productivity soar!",
+    stack: ["React", "Rails", "Javascript"],
+    sourceCode: "https://github.com/ShujaatAzim/TweedleDoo-Frontend",
+    livePreview: "https://tweedledoo.shujaatazim.com",
   },
   {
-    name: 'Projective',
+    name: "Projective",
     description:
-      'A project management app that breaks down large projects into manageable pieces. Full stack app built just in Rails!',
-    stack: ['Rails', 'Ruby'],
-    sourceCode: 'https://github.com/ShujaatAzim/Projective',
-    livePreview: 'https://projective.shujaatazim.com',
+      "A project management app that breaks down large projects into manageable pieces. Full stack app built just in Rails!",
+    stack: ["Rails", "Ruby"],
+    sourceCode: "https://github.com/ShujaatAzim/Projective",
+    livePreview: "https://projective.shujaatazim.com",
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'React',
-  'Ruby on Rails',
-  'Javascript',
-  'Ruby',
-  'Vue',
-  'HTML',
-  'CSS',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'Jest',
-  'Node.js',
-  'Recoil.js',
-  'REST API',
-  'Postgres',
-  'GraphQL',
-  'Firebase',
-  'Bootstrap',
-  'Bulma',
-  'AWS',
-  'Heroku'
+  "React",
+  "Ruby on Rails",
+  "Javascript",
+  "Ruby",
+  "Vue",
+  "HTML",
+  "CSS",
+  "Redux",
+  "SASS",
+  "Material UI",
+  "Git",
+  "Jest",
+  "Node.js",
+  "Recoil.js",
+  "REST API",
+  "Postgres",
+  "GraphQL",
+  "Firebase",
+  "Bootstrap",
+  "Bulma",
+  "AWS",
+  "Heroku"
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'shujaatazim@gmail.com',
+  email: "shujaatazim@gmail.com",
 }
 
 export { header, about, projects, skills, contact }
